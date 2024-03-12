92c3b7236afd6e3da6bba9c18f78c82349c1ec27
irpl <25093431-irpl@users.noreply.replit.com>
2024-03-12 20:23:18 +0000

    refator some stuff

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI, HTTPException, Header, Request
-from typing import Optional
+from typing import Annotated
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")  # Assuming your CSS and JS are in a 'static' folder
+app.mount("/static", StaticFiles(directory="static"),
+          name="static")  # Assuming your CSS and JS are in a 'static' folder
 
-templates = Jinja2Templates(directory="templates")  # Assuming your HTML is in a 'templates' folder
+templates = Jinja2Templates(
+    directory="templates")  # Assuming your HTML is in a 'templates' folder
 
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+  return templates.TemplateResponse("index.html", {"request": request})
+
 
 # Simulate a simple in-memory data store
 messages = {}
@@ -31,14 +34,20 @@ async def get_message(api_key: Optional[str] = Header(None)):
 
 
 @app.put("/message")
-async def update_or_create_message(message_data: dict, api_key: Optional[str] = Header(None)):
+async def update_or_create_message(message_data: dict,
+                                   api_key: Annotated[str | None,
+                                                      Header(None)] = None):
   if not api_key:
     raise HTTPException(status_code=400, detail="API key missing")
 
   if "messageLine1" not in message_data and "messageLine2" not in message_data:
-    raise HTTPException(status_code=400, detail="Missing 'message' in request body")
+    raise HTTPException(status_code=400,
+                        detail="Missing 'message' in request body")
 
-  messages[api_key] = { "line_1": message_data["messageLine1"], "line_2": message_data["messageLine2"] } 
+  messages[api_key] = {
+      "line_1": message_data["messageLine1"],
+      "line_2": message_data["messageLine2"]
+  }
   return {"status": "Message updated or created"}
 
 
