29b522f278bc82a813b2a2ebc1d731cd87d48b83
Phillip Logan <philo3194@gmail.com>
2025-03-11 22:01:35 -0500

    added a submit button

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
-const messageInput1 = document.getElementById('message-input-1');
-const messageInput2 = document.getElementById('message-input-2');
+const messageInput1 = document.getElementById("message-input-1");
+const messageInput2 = document.getElementById("message-input-2");
 const messageInputs = document.getElementsByClassName("message-input");
+const submitButton = document.getElementById("message-submit");
 var names = [
   "Abe",
   "Abraham",
@@ -272,9 +273,9 @@ function setIdentifier() {
 
 async function getMessage() {
   try {
-    const response = await fetch('/message', {
+    const response = await fetch("/message", {
       headers: {
-        'api-key': identifier,
+        "api-key": identifier,
       },
     });
     if (response.ok) {
@@ -282,36 +283,37 @@ async function getMessage() {
       if (data.line_1 || data.line_2) {
         messageInput1.value = data.line_1;
         messageInput2.value = data.line_2;
-        messageInput1.style.color = 'black'; // Set text color to black
-        messageInput2.style.color = 'black'; // Set text color to black
+        messageInput1.style.color = "black"; // Set text color to black
+        messageInput2.style.color = "black"; // Set text color to black
       } else {
-        messageInput1.value = '';
-        messageInput2.value = '';
-        messageInput1.style.color = 'inherit'; // Reset text color to default
-        messageInput2.style.color = 'inherit'; // Reset text color to default
+        messageInput1.value = "";
+        messageInput2.value = "";
+        messageInput1.style.color = "inherit"; // Reset text color to default
+        messageInput2.style.color = "inherit"; // Reset text color to default
       }
     } else {
-      console.error('Error fetching message:', response.statusText);
+      console.error("Error fetching message:", response.statusText);
     }
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 }
 
 async function handleSubmit(event) {
-  messageInput1.style.color = 'black'; // Set text color to black
-  messageInput2.style.color = 'black'; // Set text color to black
-  messageInput1.style.borderColor = 'rgba(0, 0, 0, 0)'; // Set border to green
-  messageInput2.style.borderColor = 'rgba(0, 0, 0, 0)'; // Set border to green
-  if (event.key === 'Enter') {
+  messageInput1.style.color = "black"; // Set text color to black
+  messageInput2.style.color = "black"; // Set text color to black
+  messageInput1.style.borderColor = "rgba(0, 0, 0, 0)"; // Set border to green
+  messageInput2.style.borderColor = "rgba(0, 0, 0, 0)"; // Set border to green
+  submitButton.style.backgroundColor = "#f0f0f0";
+  if (event.key === "Enter" || event.type === "click") {
     const messageLine1 = messageInput1.value.trim();
     const messageLine2 = messageInput2.value.trim();
     try {
-      const response = await fetch('/message', {
-        method: 'PUT',
+      const response = await fetch("/message", {
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json',
-          'api-key': identifier,
+          "Content-Type": "application/json",
+          "api-key": identifier,
         },
         body: JSON.stringify({ messageLine1, messageLine2 }),
       });
@@ -319,20 +321,23 @@ async function handleSubmit(event) {
         throw new Error(`Error: ${response.statusText}`);
       }
       // messageInput.style.color = 'green'; // Set text color to black
-      messageInput1.style.borderColor = 'green'; // Set border to green
-      messageInput2.style.borderColor = 'green'; // Set border to green
+      messageInput1.style.borderColor = "green"; // Set border to green
+      messageInput2.style.borderColor = "green"; // Set border to green
+      submitButton.style.backgroundColor = "green";
     } catch (error) {
       console.error(error);
       // messageInput.style.color = 'red'; // Set text color to black
-      messageInput1.style.borderColor = 'red'; // Set border to green
-      messageInput2.style.borderColor = 'red'; // Set border to green
+      messageInput1.style.borderColor = "red"; // Set border to green
+      messageInput2.style.borderColor = "red"; // Set border to green
     }
   }
 }
 
 for (var i = 0; i < messageInputs.length; i++) {
-  messageInputs[i].addEventListener('keyup', handleSubmit, false);
+  messageInputs[i].addEventListener("keyup", handleSubmit, false);
 }
+
+submitButton.addEventListener("click", handleSubmit, false);
 
 setIdentifier();
 getMessage();
